b015fa5ea0fff057e64cedbafa692df63b427442
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-12-22 06:32:40 -0300

    Enable call `/simulate` and get some dumb response

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use axum::{routing::get, Router};
+use axum::{
+    response::Json,
+    routing::{get, post},
+    Router,
+};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/simulate", post(simulate));
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // TODO: add the port on some environment variable
+    let port = 3000.to_string();
+
+    let addr = format!("0.0.0.0:{port}");
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn simulate() -> Json<Value> {
+    Json(json!({ "data": 42 }))
 }
