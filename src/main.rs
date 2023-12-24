84c79c17931dc5833c6c620c50dc7e32f6772064
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-12-24 10:32:06 -0300

    Execute the `select 1` to guarantee the connection when start

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,34 @@ use axum::{
     Router,
 };
 use serde_json::{json, Value};
+use tokio_postgres::NoTls;
+
+mod embedded {
+    use refinery::embed_migrations;
+    embed_migrations!("./infraestructure/database/migrations");
+}
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+    let conn = "host=localhost user=sim password=sim dbname=sim";
+    let (client, connection) = tokio_postgres::connect(conn, NoTls)
+        .await
+        .expect("cannot connect into database");
+
+    // Spawn a new tokio runtime to handle the connection
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Now we can execute a simple statement that just returns its parameter, just to make sure
+    // the database connect successfully
+    client
+        .query("SELECT 1", &[])
+        .await
+        .expect("error occurred trying to run `select 1` on postgres");
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/simulate", post(simulate));
